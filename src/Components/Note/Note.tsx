662d13273b1ae36cf7a37286a2bd2d75e0c07cc5
Nicole Stanton <n.stanton14@ymail.com>
2025-02-06 21:46:11 +0000

    add save functionality on page refresh

--- src/Components/Note/Note.tsx
+++ src/Components/Note/Note.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./Note.module.scss";
 
 type Note = {
@@ -7,33 +7,45 @@ type Note = {
   value: string;
 };
 
+type NoteResponse = {
+  id: number | null;
+  body: string;
+};
+
 export const Note = () => {
   const [noteData, setNoteData] = useState<Note>({ id: null, value: "" });
+  const [latestNote, setLatestNote] = useState<NoteResponse | null>(null);
   const saveTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const SESSION_NAME = "session1";
+  const SESSION_NAME = "session3";
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const response = await fetch(
+        `https://challenge.surfe.com/${SESSION_NAME}/notes`
+      );
+      const data = await response.json();
+      setLatestNote(data[0]);
+    };
+    fetchNotes();
+  }, []);
 
   const saveNote = async (content: string) => {
     try {
       let url = `https://challenge.surfe.com/${SESSION_NAME}/notes`;
-      const method = noteData.id ? "PUT" : "POST";
+      const method = latestNote ? "PUT" : "POST";
 
-      if (noteData.id) {
-        url = `${url}/${noteData.id}`;
+      if (latestNote) {
+        url = `${url}/${latestNote.id}`;
       }
 
       const response = await fetch(url, {
         method,
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ id: noteData.id, body: content }),
+        body: JSON.stringify({ id: latestNote?.id, body: content }),
       });
 
       if (!response.ok)
         throw new Error(`Failed to ${noteData.id ? "update" : "save"} note`);
-
-      const data = await response.json();
-      if (!noteData.id && data.id) {
-        setNoteData({ id: data.id, value: content });
-      }
     } catch (error) {
       console.error("Error saving note:", error);
     }
@@ -54,13 +66,11 @@ export const Note = () => {
     saveContentAfterDelay(e.target.value);
   };
 
-  console.log("noteData", noteData);
-
   return (
     <div className={styles.note}>
       <span>Note</span>
       <textarea
-        defaultValue={noteData.value}
+        defaultValue={latestNote?.body}
         placeholder="add a note"
         onChange={handleChange}
       />
