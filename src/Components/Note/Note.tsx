66fe42105021381b709119f475354de4cbdfd298
Nicole Stanton <n.stanton14@ymail.com>
2025-02-06 20:34:15 +0000

    send note content and id to endpoint

--- src/Components/Note/Note.tsx
+++ src/Components/Note/Note.tsx
@@ -1,25 +1,66 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./Note.module.scss";
 
 type Note = {
-  id: number;
+  id: number | null;
   value: string;
 };
 
 export const Note = () => {
-  const [noteValue, setNoteValue] = useState("");
+  const [noteData, setNoteData] = useState<Note>({ id: null, value: "" });
+  const saveTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const SESSION_NAME = "session1";
+
+  const saveNote = async (content: string) => {
+    try {
+      let url = `https://challenge.surfe.com/${SESSION_NAME}/notes`;
+      const method = noteData.id ? "PUT" : "POST";
+
+      if (noteData.id) {
+        url = `${url}/${noteData.id}`;
+      }
+
+      const response = await fetch(url, {
+        method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: noteData.id, body: content }),
+      });
+
+      if (!response.ok)
+        throw new Error(`Failed to ${noteData.id ? "update" : "save"} note`);
+
+      const data = await response.json();
+      if (!noteData.id && data.id) {
+        setNoteData({ id: data.id, value: content });
+      }
+    } catch (error) {
+      console.error("Error saving note:", error);
+    }
+  };
+
+  const saveContentAfterDelay = (content: string) => {
+    if (saveTimeoutRef.current) {
+      clearTimeout(saveTimeoutRef.current);
+    }
+    saveTimeoutRef.current = setTimeout(() => {
+      saveNote(content);
+    }, 1000);
+  };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleChange = (e: any) => {
-    setNoteValue(e.target.value);
+    setNoteData({ id: noteData.id, value: e.target.value });
+    saveContentAfterDelay(e.target.value);
   };
+
+  console.log("noteData", noteData);
 
   return (
     <div className={styles.note}>
       <span>Note</span>
       <textarea
-        defaultValue={noteValue}
+        defaultValue={noteData.value}
         placeholder="add a note"
         onChange={handleChange}
       />
